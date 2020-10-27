4f9fa5a5963c18c48eb01b6d1036c6b97f914415
Alexander O'Mara <me@alexomara.com>
2020-10-26 20:29:56 -0400

    Simplified the options code

--- js/src/admin/components/WPUsersSettingsModal.js
+++ js/src/admin/components/WPUsersSettingsModal.js
@@ -13,71 +13,28 @@ export class WPUsersSettingsModal extends SettingsModal {
 
 	form() {
 		const setting = key => this.setting(`${ID}.${key}`);
+		const option = (key, name, info) => (
+			<div className="Form-group">
+				<label>{name} (<code>{info}</code>)</label>
+				<code>
+					<input className="FormControl" bidi={setting(key)}/>
+				</code>
+			</div>
+		);
 		return [
-			<div className="Form-group">
-				<label>Login URL (<code>.../wp-login.php</code>)</label>
-				<code><input className="FormControl" bidi={setting('login_url')}/></code>
-			</div>
-			,
-			<div className="Form-group">
-				<label>Profile URL (<code>.../wp-admin/profile.php</code>)</label>
-				<code><input className="FormControl" bidi={setting('profile_url')}/></code>
-			</div>
-			,
-			<div className="Form-group">
-				<label>Cookie Name (<code>wordpress_logged_in_*</code>)</label>
-				<code><input className="FormControl" bidi={setting('cookie_name')}/></code>
-			</div>
-			,
-			<div className="Form-group">
-				<label>Database Host (<code>DB_HOST</code>)</label>
-				<code><input className="FormControl" bidi={setting('db_host')}/></code>
-			</div>
-			,
-			<div className="Form-group">
-				<label>Database User (<code>DB_USER</code>)</label>
-				<code><input className="FormControl" bidi={setting('db_user')}/></code>
-			</div>
-			,
-			<div className="Form-group">
-				<label>Database Password (<code>DB_PASSWORD</code>)</label>
-				<code><input className="FormControl" bidi={setting('db_pass')}/></code>
-			</div>
-			,
-			<div className="Form-group">
-				<label>Database Name (<code>DB_NAME</code>)</label>
-				<code><input className="FormControl" bidi={setting('db_name')}/></code>
-			</div>
-			,
-			<div className="Form-group">
-				<label>Database Charset (<code>DB_CHARSET</code>)</label>
-				<code><input className="FormControl" bidi={setting('db_charset')}/></code>
-			</div>
-			,
-			<div className="Form-group">
-				<label>Database Prefix (<code>$table_prefix</code>)</label>
-				<code><input className="FormControl" bidi={setting('db_pre')}/></code>
-			</div>
-			,
-			<div className="Form-group">
-				<label>Logged In Key (<code>LOGGED_IN_KEY</code>)</label>
-				<code><input className="FormControl" bidi={setting('logged_in_key')}/></code>
-			</div>
-			,
-			<div className="Form-group">
-				<label>Logged In Salt (<code>LOGGED_IN_SALT</code>)</label>
-				<code><input className="FormControl" bidi={setting('logged_in_salt')}/></code>
-			</div>
-			,
-			<div className="Form-group">
-				<label>Nonce Key (<code>NONCE_KEY</code>)</label>
-				<code><input className="FormControl" bidi={setting('nonce_key')}/></code>
-			</div>
-			,
-			<div className="Form-group">
-				<label>Nonce Salt (<code>NONCE_SALT</code>)</label>
-				<code><input className="FormControl" bidi={setting('nonce_salt')}/></code>
-			</div>
+			option('login_url', 'Login URL', '.../wp-login.php'),
+			option('profile_url', 'Profile URL', '.../wp-admin/profile.php'),
+			option('cookie_name', 'Cookie Name', 'wordpress_logged_in_...'),
+			option('db_host', 'Database Host', 'DB_HOST'),
+			option('db_user', 'Database User', 'DB_USER'),
+			option('db_pass', 'Database Password', 'DB_PASSWORD'),
+			option('db_name', 'Database Name', 'DB_NAME'),
+			option('db_charset', 'Database Charset', 'DB_CHARSET'),
+			option('db_pre', 'Database Prefix', '$table_prefix'),
+			option('logged_in_key', 'Logged In Key', 'LOGGED_IN_KEY'),
+			option('logged_in_salt', 'Logged In Salt', 'LOGGED_IN_SALT'),
+			option('nonce_key', 'Nonce Key', 'NONCE_KEY'),
+			option('nonce_salt', 'Nonce Salt', 'NONCE_SALT')
 		];
 	}
 }
