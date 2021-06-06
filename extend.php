fd047549cbb87597fbbcedaa966363394c6cfa3a
Alexander O'Mara <me@alexomara.com>
2021-06-06 14:52:14 -0400

    Removed obsolete compat layer

--- extend.php
+++ extend.php
@@ -6,7 +6,6 @@ use Flarum\User\Event\Saving;
 use Flarum\User\Search\UserSearcher;
 use Illuminate\Contracts\Events\Dispatcher;
 
-use AlexanderOMara\FlarumWPUsers\Compat;
 use AlexanderOMara\FlarumWPUsers\Core;
 use AlexanderOMara\FlarumWPUsers\Extenders;
 use AlexanderOMara\FlarumWPUsers\Events;
@@ -59,6 +58,6 @@ return [
 			Core::DISPLAY_NAME_DRIVER,
 			DisplayName\Driver::class
 		),
-	(new Compat\SimpleFlarumSearch(UserSearcher::class))
+	(new Extend\SimpleFlarumSearch(UserSearcher::class))
 		->setFullTextGambit(DisplayName\FullTextGambit::class)
 ];
